82f7d251152039f5119231278221839840cf6a18
Noah Nichols <noahjacknichols@gmail.com>
2019-02-05 23:13:46 -0500

    added assignment files

---
+++ birthday.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+int calculateBirthday(int day, int month){
+
+}
+
+
+int main(int argc, char *argv[]){
+
+    char whoDate[] = system("who"); //find easy way to delimit this
+    printf("Enter your day of birth (DD)");
+    int day = scanf("%d");
+    print("Enter your month of birth (MM)");
+    int month = scanf("%d");
+    int birthday = calculateBirthday(day, month);
+    
+
+}
