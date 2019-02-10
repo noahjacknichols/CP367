338b9e99141e0c625c0c036c6d4c1996115f241d
Noah Nichols <noahjacknichols@gmail.com>
2019-02-10 16:28:15 -0500

    .

--- birthday.c
+++ birthday.c
@@ -6,15 +6,15 @@
  #include <sys/types.h>
  #include <unistd.h>
  #include <math.h>
+
  const long int YEAR_IN_SECONDS = 31556926;
+const long int GMT = 14 * 3600;
 
-
-long int calculateBirthday(long int day, long int month, long int currentYear){
+long int calculateEpoch(long int day, long int month){
     //calculates the epochTime from the given day and month
     printf("day:%ld\n", day);
     printf("month:%ld\n", month);
-    printf("Year:%ld\n", 1970+currentYear);
-    long int epochTime = day * 86400 + month * 2629743 + currentYear * YEAR_IN_SECONDS;
+    long int epochTime = (day) * 86400 + (month) * 2629743;
     return epochTime;
 
 }
@@ -22,20 +22,76 @@ long int calculateBirthday(long int day, long int month, long int currentYear){
 long int days(long int a, long int b){
     printf("a:%ld\n", a);
     printf("b: %ld\n", b);
-    long int timeLeft = abs(a -b);
-    long int days = ceil((((timeLeft)/60)/60)/24);
+    long int timeLeft = a-b;
+    printf("timeLeft:%ld\n", a-b);
+    int days = ceil((((timeLeft)/60)/60));
+    printf("days:%d, calculated:\n", days);
     return days;
+}
+
+int calculateBirthday(int day, int month, int sysday, int sysmonth, int *months){
+    int found = 0;
+    int days = 0;
+    int i = sysmonth;
+    int j = sysday;
+    int wrapAround = 0;
+    int newMonth = 0;
+    printf("day:%d, month %d, sysday:%d, sysmonth:%d\n", day,month,sysday,sysmonth);
+    while(!found){
+        if(wrapAround){
+            i = 1;
+        }
+        for(i; i < 12; i++){
+            if(newMonth){
+                j = 1;
+                days++;
+            }
+            for(j; j < months[i-1]; j++){
+                printf("i:%d, j:%d\n", i,j);
+                if(i == month && j == day){
+
+                    return days; //if we are on the birthday, return days
+                }
+                if(!found){
+                    days++;
+                }
+                newMonth = 1;
+            }
+        }
+        wrapAround = 1;
+    }
 }
 
 
 int main(int argc, char *argv[]){
+    int months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     time_t timeInSeconds;
     
-    timeInSeconds = time(NULL);
 
-    //uid_t id = geteuid(getuid());
+
+    long int offset = 0;
+    timeInSeconds = time(NULL);
+    if(argc > 1){
+        offset = 86400 * atoi(argv[1]);
+        timeInSeconds = timeInSeconds + offset;
+    }
+
+    long int temp = timeInSeconds;
+    int year = 0;
+    while(temp > 0){
+        year++;
+        temp -= YEAR_IN_SECONDS;
+    }
+    year += 1970;
+    if(year % 400 == 0 && year % 100 != 0){
+        months[1] = 29; //account for leap year 
+    }
     char buf[32];
+    char buf2[32];
     char userID[32];
+    char birthdayEpoch[32];
+    char currentEpoch[32];
+
     size_t size = 32;
     //greet user
     getlogin_r(userID, size);
@@ -44,35 +100,52 @@ int main(int argc, char *argv[]){
     //print date with or without offset
     sprintf(buf, "date -d @%d", timeInSeconds);
     system(buf);
-    long int temp = timeInSeconds;
-    long int currentYear = 0;
-    while(temp - YEAR_IN_SECONDS > 0){
-        currentYear++;
-        temp = temp - YEAR_IN_SECONDS;
-    }
-    //printf("calculated currentYear to be: %d\n", currentYear);
     
+
+
+    //prompt for user input
     long int day, month;
     printf("Enter your day of birth (DD):");
     scanf("%ld", &day);
     printf("Enter your month of birth (MM):");
     scanf("%ld", &month);
 
+    snprintf(birthdayEpoch, 32, "%d-%d-%d", month, day, year); //get mm-dd-yyyy format 
+    sprintf(buf2, "date --d %s +'%s'");
+
     if(month <= 12 && day <= 32){ //user input is correct
-        long int birthday = calculateBirthday(day, month, currentYear);
-        if(days(birthday, timeInSeconds) == 0){
+        
+        long int strippedEpoch = timeInSeconds % 31556926; //strip epoch time of the years for convenience
+        long int sysmonth = ceil(strippedEpoch/3600/24/12) -1; //calculate # months, subtract 1 to account for correct month format
+        long int sysday = ceil((strippedEpoch%2629743)/86400); //calculate # days
+        printf("calculated month:%ld, day:%ld\n", sysmonth,sysday);
+        int days = calculateBirthday(day, month, sysday, sysmonth, months);
+        if(days == 0){
             printf("Happy Birthday, %s!\n", userID);
         }else{
-            long int untilBirthday = 0; //initalize value in case
-            if(birthday > timeInSeconds){
-                printf("birthday > time\n");
+            // long int untilBirthday = 0; //initalize value in case
+            //     long int systime = calculateEpoch(sysday, sysmonth);
+            //     long int time = calculateEpoch(day,month);
+            // int isBehind = 0;
+            // if(time < systime){ //if the current day is after the birthday, we have a different handler
+            //     isBehind = 1;
+            // }
+            // if(isBehind){ 
+            //     //the birthday is behind todays date
+            //     printf("birthday < time\n");
+            //     systime = calculateEpoch(sysday, sysmonth);
+            //     month+=12; // offset by a year
+            //     time = calculateEpoch(day,month);
                 
-                untilBirthday = days(birthday, timeInSeconds);
-            }else{
-                printf("birthday < time\n");
-                untilBirthday = days(birthday + YEAR_IN_SECONDS, timeInSeconds);
-            }
-            printf("There are %ld days until your birthday.\n", untilBirthday);
+            //     untilBirthday = (time - systime)/86400;
+            // }else{
+            //     //birthday after today's date
+            //     systime = calculateEpoch(sysday, sysmonth);
+            //     time = calculateEpoch(day,month);
+            //     untilBirthday = (time - systime)/86400;
+            // }
+            
+            printf("There are %ld days until your birthday.\n", days);
         }
     }else{
         printf("Sorry, it seems the date you put in is invalid.\n");
